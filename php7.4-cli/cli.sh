f374c3df76bed37ae85c5a6dfe52e893886c9296
Mohamad Supangat <moha.supangat@gmail.com>
2025-05-16 14:12:56 +0700

    Menggunakan current userid ke parameter user pada docker run

--- php7.4-cli/cli.sh
+++ php7.4-cli/cli.sh
@@ -1,1 +1,6 @@
-docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -p 8000:8000 phpdockerio/php74-cli $@
+#!/usr/bin/bash
+
+UID=$(id -u)
+GID=$(id -g)
+
+docker run -it --user "$UID:$GID" --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -p 8000:8000 phpdockerio/php74-cli $@
--- php8.3-cli/cli.sh
+++ php8.3-cli/cli.sh
@@ -1,1 +1,6 @@
-docker run -it --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -p 8000:8000 phpdockerio/php:8.3-cli $@
+#!/usr/bin/bash
+
+UID=$(id -u)
+GID=$(id -g)
+
+docker run -it --user "$UID:$GID" --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -p 8000:8000 phpdockerio/php:8.3-cli $@
