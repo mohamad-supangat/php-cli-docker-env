530bb9e51ca79f1909a69a5430abc4bddc261d2a
Mohamad Supangat <moha.supangat@gmail.com>
2025-05-16 14:15:59 +0700

    fix: memperbaiki uid readonly

--- php7.4-cli/cli.sh
+++ php7.4-cli/cli.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/bash
-
-UID=$(id -u)
-GID=$(id -g)
-
-docker run -it --user "$UID:$GID" --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp -p 8000:8000 phpdockerio/php74-cli $@
+docker run -it --user "$(id -u):$(id -g)" --rm -v "$PWD:/usr/src/myapp" -w /usr/src/myapp -p 8000:8000 phpdockerio/php74-cli "$@"
