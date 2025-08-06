3e4e24eb46978c3b44d94a3ba74aaff4d64442df
Mohamad Supangat <moha.supangat@gmail.com>
2025-08-06 11:38:45 +0700

    7.4: add wkhtml2pdf path

--- 7.4/cli.sh
+++ 7.4/cli.sh
@@ -1,1 +1,1 @@
-docker run -it --user "$(id -u):$(id -g)" --rm -v "$PWD:/usr/src/myapp" -v "$HOME/.config/composer":/.composer -w /usr/src/myapp -p 8000:8000 phpdockerio/php74-cli "$@"
+docker run -it --rm -v "$PWD:/usr/src/myapp" -v "$HOME/.config/composer":/.composer -w /usr/src/myapp -p 8000:8000 phpdockerio/php74-cli "$@"
