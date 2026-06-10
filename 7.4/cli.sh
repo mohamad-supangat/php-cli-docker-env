cc88711febefe28d48192dd82894b84378a49b91
Mohamad Supangat <moha.supangat@gmail.com>
2026-06-10 14:50:25 +0700

    feat: cli

--- 7.4/cli.sh
+++ 7.4/cli.sh
@@ -1,1 +1,25 @@
-docker run -it --rm --name="php74" -v "$PWD:/usr/src/myapp" -v "$HOME/.config/composer":/.composer -w /usr/src/myapp -p 8000:8000 php74-cli "$@"
+#!/bin/bash
+
+PORT=""
+REMAINING_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --port)
+    PORT="$2"
+    shift 2
+    ;;
+  *)
+    REMAINING_ARGS+=("$1")
+    shift
+    ;;
+  esac
+done
+
+DOCKER_OPTS=(-it --rm -v "$HOME/.config/composer":/.composer -v "$PWD:/usr/src/myapp" -w /usr/src/myapp)
+
+if [[ -n "$PORT" ]]; then
+  DOCKER_OPTS+=(-p "$PORT:$PORT")
+fi
+
+docker run "${DOCKER_OPTS[@]}" php74-cli "${REMAINING_ARGS[@]}"
